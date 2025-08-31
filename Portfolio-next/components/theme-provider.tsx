fb980f444297e577effb247f3b648d6540126350
Juan Felipe Hurtado <jfhvjfhv0015@gmail.com>
2025-08-31 10:34:27 -0500

    Refactor ThemeProvider component to define a more flexible props interface, allowing for additional properties while maintaining children as a required prop.

--- Portfolio-next/components/theme-provider.tsx
+++ Portfolio-next/components/theme-provider.tsx
@@ -2,7 +2,11 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  [key: string]: any
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
